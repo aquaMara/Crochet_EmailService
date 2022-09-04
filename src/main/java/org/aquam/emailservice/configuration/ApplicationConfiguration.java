28964a7d3c3a82b79f898b08fecddcd980d4df0f
Marina Tchernyshenko <aquamarina.na@gmail.com>
2022-09-05 00:07:54 +0300

    feat: Add configuration for FreeMarker

--- src/main/java/org/aquam/emailservice/configuration/ApplicationConfiguration.java
+++ src/main/java/org/aquam/emailservice/configuration/ApplicationConfiguration.java
@@ -3,8 +3,10 @@ package org.aquam.emailservice.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
 
 import java.util.Properties;
 
@@ -36,5 +38,13 @@ public class ApplicationConfiguration {
         mailSender.setJavaMailProperties(javaMailProperties);
 
         return mailSender;
+    }
+
+    @Primary
+    @Bean
+    public FreeMarkerConfigurationFactoryBean factoryBean() {
+        FreeMarkerConfigurationFactoryBean bean = new FreeMarkerConfigurationFactoryBean();
+        bean.setTemplateLoaderPath("classpath:/templates");
+        return bean;
     }
 }
