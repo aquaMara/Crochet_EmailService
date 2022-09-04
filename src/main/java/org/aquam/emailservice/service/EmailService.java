397f956780cff025e613f0f3af7e2d56b500acaf
Marina Tchernyshenko <aquamarina.na@gmail.com>
2022-09-05 00:19:16 +0300

    feat: Add freemarker template and logger

--- src/main/java/org/aquam/emailservice/service/EmailService.java
+++ src/main/java/org/aquam/emailservice/service/EmailService.java
@@ -5,4 +5,5 @@ import org.aquam.emailservice.model.ConfirmationData;
 public interface EmailService {
 
     String sendEmail(ConfirmationData confirmationData);
+    String createMessageBody(String link);
 }
--- src/main/java/org/aquam/emailservice/service/impl/EmailServiceImpl.java
+++ src/main/java/org/aquam/emailservice/service/impl/EmailServiceImpl.java
@@ -3,46 +3,54 @@ package org.aquam.emailservice.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.aquam.emailservice.model.ConfirmationData;
 import org.aquam.emailservice.service.EmailService;
+freimport org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.util.HashMap;
+import java.util.Map;
+
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 @Service
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
 
     private final JavaMailSender mailSender;
+    private final ApplicationConfiguration applicationConfiguration;
+    private static Logger logger = LoggerFactory.getLogger(EmailServiceImpl.class);
 
     @Override
     public String sendEmail(ConfirmationData confirmationData) {
         try {
             MimeMessage mimeMessage = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
-            helper.setText(createMessage(confirmationData.getConfirmationLink()), true);
+            helper.setText(createMessageBody(confirmationData.getConfirmationLink()), true);
             helper.setTo(confirmationData.getEmail());
             helper.setSubject("Confirmation email");
             mailSender.send(mimeMessage);
-        } catch (MessagingException e) {
-            throw new IllegalStateException("failed to send email");
         } catch (Exception e) {
-            throw new IllegalStateException("failed to send email");
+            logger.error("Class: " + e.getClass() + " Message: " + e.getMessage() + " User email: " + confirmationData.getEmail());
+            throw new EmailSendingException("Failed to send email, check email address");
         }
 
         return "email to address " + confirmationData.getEmail() + " was sent";
     }
 
-    public String createMessage(String link) {
-
-        return "<div style=\"margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">\n" +
-                "    <p>Hey, thank you for registering :)</p>\n" +
-                "    <p>Please click on the link below to activate your account</p>\n" +
-                "    <p style=\"margin:0 0 20px 0;font-size:19px;line-height:25px;color:#0b0c0c\">\n" +
-                "        <a href=\"" + link + "\">Activate account</a>\n" +
-                "    </p>\n" +
-                "    <p>See you soon!</p>\n" +
-                "</div>";
+    @Override
+    public String createMessageBody(String link) {
+        Template template = applicationConfiguration.factoryBean().createConfiguration().getTemplate("email.ftlh");
+        Map<String, String> model = new HashMap<>();
+        model.put("name", "NNNN");
+        model.put("username", "NNNN");
+        model.put("link", "https://yandex.by/");
+        return htmlBody = FreeMarkerTemplateUtils.processTemplateIntoString(template, model);
     }
 }
